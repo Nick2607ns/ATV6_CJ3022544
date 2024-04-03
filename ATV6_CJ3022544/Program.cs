ff5b71a5c5d53e42c4096731e4d7081f29953243
CJ3022544 <CJ3022544@LAB03-PC02>
2024-04-03 10:15:37 -0300

    Adicionar arquivos de projeto.

---
+++ ATV6_CJ3022544/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+//exercicio 1
+/*
+int num, i=0;
+
+Console.WriteLine("digite um num inteiro");
+num = int.Parse(Console.ReadLine());
+
+
+while ( i <= num)
+{
+    Console.WriteLine(i);
+    i++;
+
+}
+*/
+
+// exercicio 2
+
+/*
+int num, i = 0;
+
+Console.WriteLine("digite um num inteiro");
+num = int.Parse(Console.ReadLine());
+
+
+while (i <= num)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        
+    }
+    i++;
+}
+
+*/
+
+//exercicio 3
+/*
+int i, num = 1000;
+
+Console.WriteLine("digite um numero inteiro");
+i= int.Parse(Console.ReadLine());
+
+while ( i <= num)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+
+*/
+
+// exercicio 4
+
+int soma = 0, i = 1;
+
+Console.WriteLine("digite um numero inteiro positivo");
+soma = int.Parse(Console.ReadLine());
+
+
+
