83caf80f220806d7d9cd1189f6a77ac1098da323
Nick2607ns <162454343+Nick2607ns@users.noreply.github.com>
2024-04-10 09:38:49 -0300

    Update Program.cs

--- ATV6_CJ3022544/Program.cs
+++ ATV6_CJ3022544/Program.cs
@@ -60,10 +60,86 @@ while ( i <= num)
 
 // exercicio 4
 
-int soma = 0, i = 1;
+int n1, a1 = 1;
+int soma = 200;
+Console.WriteLine("Digite um número");
+n1 = int.Parse(Console.ReadLine());
+    while (a1 < soma)
+{
+    if (n1 < 0)
+    {
+        Console.WriteLine("número inválido");
+    }
+    else
+    {
+        Console.WriteLine(a1);
+    }
+    a1++;
+}
 
-Console.WriteLine("digite um numero inteiro positivo");
-soma = int.Parse(Console.ReadLine());
+
+
+// exercicio 5
+
+    int v, p1 = 0;
+    Console.WriteLine("Digite um número:");
+    v = int.Parse(Console.ReadLine());
+
+    while (p1 <= v)
+    {
+        if (v % p1 == 0)
+        {
+            Console.WriteLine(p1);
+        }
+        p1++;
+    };
+
+    
+
+// exercicio 6
+
+    int j1 = 0, v2;
+    while (j1 < 11)
+    {
+        Console.WriteLine("Digite um valor");
+        v2 = int.Parse(Console.ReadLine());
+        if (v2 < v2)
+        {
+            Console.WriteLine("o número maior é = {0}", v2);
+        }
+        if (v2 > v2)
+        {
+            Console.WriteLine("o número menor é = {0}", v2);
+        }
+        j1++;
+    };
+
+
+
+// exercicio 7
+
+
+                        int t = 0, l3;
+    int r1 = 0, p3 = 0;
+    while (t < 10)
+    {
+        Console.WriteLine("Digite um valor");
+        l3 = int.Parse(Console.ReadLine());
+        if (l3 <= 0)
+            break;
+        if (t % 2 != 0)
+        {
+            r1 = r1 + t;
+        }
+        if (t % 2 == 0)
+        {
+            p3 = p3 + t;
+        }
+        Console.WriteLine("soma dos números ímpares = {0}", r1);
+        Console.WriteLine("soma dos números pares = {0}", p3);
+        t++;
+    }
+
 
 
 
